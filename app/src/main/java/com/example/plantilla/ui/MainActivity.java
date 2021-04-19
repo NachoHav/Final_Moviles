779a57221a51c5b26a07b473fbcc050b550866d8
Gilberto Hernandez Amura <ghernandezamura@gmail.com>
2021-04-19 16:41:28 -0300

    Funcionalidad llamada con sensor en Login

--- app/src/main/java/com/example/plantilla/ui/MainActivity.java
+++ app/src/main/java/com/example/plantilla/ui/MainActivity.java
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,15 +24,22 @@ import android.widget.Toast;
 
 import com.example.plantilla.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener
+{
     private EditText etUsuario, etContrasena;
     private Button btnLogin;
     private MainActivityViewModel mainActivityViewModel;
+    private SensorManager sensorManager;
+    private List<Sensor> sensores;
+    private boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pedirPermisos();
 
         mainActivityViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
         inicializarComponentes();
@@ -60,6 +76,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensores = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if(sensores.size() > 0){
+            sensorManager.registerListener(this, sensores.get(0), SensorManager.SENSOR_DELAY_GAME);
+        }
+        flag = true;
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        sensorManager.unregisterListener(this);
+    }
+
+    private void pedirPermisos(){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE},1000);
+        }
+    }
+
     private void inicializarComponentes() {
         etUsuario = findViewById(R.id.etUsuario);
         etContrasena = findViewById(R.id.etContrasena);
@@ -76,4 +119,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        float x = event.values[0];
+        Log.d("msj", x + "");
+
+        if(flag && (x > 10 || x < -10)){
+            Intent i = new Intent(Intent.ACTION_CALL);
+            i.setData(Uri.parse("tel: 555"));
+            startActivity(i);
+            flag = false;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }
 }
