fb82d6ad6d52b627e03e8d961a3d78cfd498061a
Gilberto Hernandez Amura <ghernandezamura@gmail.com>
2021-04-19 19:17:26 -0300

    Renombre referencias del menu

--- app/src/main/java/com/example/plantilla/ui/ui/perfil/PerfilFragment.java
+++ app/src/main/java/com/example/plantilla/ui/ui/perfil/PerfilFragment.java
@@ -19,7 +19,7 @@ public class PerfilFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         perfilViewModel = new ViewModelProvider(this).get(PerfilViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        View root = inflater.inflate(R.layout.fragment_perfil, container, false);
 
         return root;
     }
